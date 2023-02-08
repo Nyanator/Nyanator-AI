7f0c5c2fad903d875a5e2de637205bc75035f3a2
Nyanator <asaharatoru@gmail.com>
2023-02-08 22:19:50 +0900

    post test

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Message(BaseModel):
     user_id: str
     content: str
 
-@app.post('/message/')
+
+@app.post('/talk/')
 async def talk(message: Message):
-    return {"res":"okok","ID":message.user_id,"name":message.content}
+    return {"res": "okok", "ID": message.user_id, "name": message.content}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
